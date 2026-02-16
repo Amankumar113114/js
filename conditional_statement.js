6ddc71c859f501fb30cf742baf691c8d9dc9610b
Aman Kumar <27aman113114@gmil.com>
2026-02-16 20:40:05 +0530

    c_statement

---
+++ conditional_statement.js
@@ -0,0 +1,18 @@
+let marks=80;
+if (marks>30){
+    console.log("pass")
+}
+else{
+    console.log("fail")
+}
+
+// if else ladder or nested condition
+if(marks>=90){
+    console.log("A+");
+}
+else if(marks>=30){
+    console.log("B+");
+}
+else{
+    console.log("fail");
+}
