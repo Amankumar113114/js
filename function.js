2957e36300cf95b83fdab3d89f6f53b8a6417868
Aman Kumar <27aman113114@gmil.com>
2026-02-17 22:29:37 +0530

    function

---
+++ function.js
@@ -0,0 +1,14 @@
+//functions ===> for reuse the code
+function sum(a,b){
+     sum=a+b
+    console.log("the sum is" + sum)
+    return a+b
+}
+result=sum(3,5)
+console.log(result)
+
+// arrow func
+let fun1=(x)=>{
+    console.log(x)
+}
+fun1(34)
